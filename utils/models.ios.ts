cde52fd7861239bf6264863bcab4b3bf4e3f2c80
wcyat <wcyat@wcyat.me>
2024-01-29 21:09:35 +0800

    models: support ios and android

---
+++ utils/models.ios.ts
@@ -0,0 +1,5 @@
+export const modelPath = {
+  "whisper-small-yue-mdcc": require("../assets/models/whisper-small-yue-mdcc.bin"),
+  "whisper-small-yue-hk": require("../assets/models/whisper-small-yue-hk.bin"),
+  "whisper-base-yue-hk-mdcc": require("../assets/models/whisper-base-yue-hk-mdcc.bin"),
+};
